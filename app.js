f6df1862a09e9dd528d9d849f9abdb84621dea86
lolulo69 <lefortlucas1@gmail.com>
2022-12-02 14:59:16 +0100

    Premier upload

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const path = require('path');
+const userRoutes = require('./routes/user');
+const sauceRoutes = require('./routes/sauce');
+
+dotenv.config();
+
+const app = express();
+mongoose.connect('mongodb+srv://'+process.env.DATABASE_KEY+'@cluster0.xhh06do.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true, dbName: "OCR_Projet6"})
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use('/api/auth', userRoutes);
+
+app.use('/api/sauces', sauceRoutes);
+
+module.exports = app;
